8079d3212f6ba0c4e7657425420666e2280b3767
glory kananga <glodamour16@gmail.com>
2022-06-25 21:50:47 +0200

    AVL tree

---
+++ src/main/java/com/mycompany/avl/Node.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.avl;
+
+/**
+ *
+ * @author glorymutombo
+ */
+public class Node {
+    int key, height;
+Node left, right;
+Node(int d) {
+key = d;
+height = 1;
+}
+
+
+}
